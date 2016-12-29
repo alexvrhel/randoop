6018fae5202292830e50c2f60089749856667bb8
Ben Keller <bjkeller@cs.washington.edu>
2016-12-29 12:04:22 -0800

    Change example to use updated condition method sigantures

--- src/testinput/java/net_Connection.java
+++ src/testinput/java/net_Connection.java
@@ -6,32 +6,32 @@
 public class net_Connection {
 
     // void net.Connection.open()
-    public static boolean m1_t0(Object receiver, Object... args) {
+    public static boolean m1_t0(net.Connection target) {
         // @throws java.lang.IllegalStateException if the connection is already open ==> target.isOpen()
-        return ((net.Connection) receiver).isOpen();
+        return target.isOpen();
     }
 
     // void net.Connection.send(java.lang.String message)
-    public static boolean m2_t0(Object receiver, Object... args) {
+    public static boolean m2_t0(net.Connection target, java.lang.String message) {
         // @throws java.lang.NullPointerException if message is null ==> args[0]==null
-        return ((java.lang.String) args[0]) == null;
+        return message == null;
     }
 
     // void net.Connection.send(java.lang.String message)
-    public static boolean m2_t1(Object receiver, Object... args) {
+    public static boolean m2_t1(net.Connection target, java.lang.String message) {
         // @throws java.lang.IllegalStateException if the connection is not open ==> (target.isOpen()) == false
-        return (((net.Connection) receiver).isOpen()) == false;
+        return (target.isOpen()) == false;
     }
 
     // void net.Connection.send(int code)
-    public static boolean m3_t0(Object receiver, Object... args) {
+    public static boolean m3_t0(net.Connection target, int code) {
         // @throws java.lang.IllegalStateException if the connection is not open ==> (target.isOpen()) == false
-        return (((net.Connection) receiver).isOpen()) == false;
+        return (target.isOpen()) == false;
     }
 
     // void net.Connection.send(int code)
-    public static boolean m3_p0(Object receiver, Object... args) {
+    public static boolean m3_p0(net.Connection target, int code) {
         // @param code the code, must be positive ==> args[0]>0
-        return ((int) args[0]) > 0;
+        return code > 0;
     }
 }
